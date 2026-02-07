5a73af5952bc593ace114cbe969987dadd97ee84
AbuCTF <aburahman918@gmail.com>
2026-02-07 23:15:43 +0530

    v0.83

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,43 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		}),
+		compression({
+			algorithm: 'gzip',
+			ext: '.gz'
+		}),
+		compression({
+			algorithm: 'brotliCompress',
+			ext: '.br'
+		})
+	],
 	server: {
 		port: 3000,
 		host: true
+	},
+	build: {
+		minify: 'terser',
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true
+			}
+		},
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					'svelte-vendor': ['svelte', '@sveltejs/kit'],
+					'bits-ui': ['bits-ui']
+				}
+			}
+		},
+		chunkSizeWarningLimit: 1000
 	}
 });
